500f69024a4dc8f948275b22e6124d7c1103bf75
Stryker Buffington <sbuffing@ucsc.edu>
2021-06-08 14:00:18 -0700

    laoding bar

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -5,6 +5,45 @@ class MainMenu extends Phaser.Scene {
     }
 
     preload () {
+        if (this.play){
+            return;
+        }
+
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBar.x = 240;
+        progressBox.x = 240;
+        progressBar.y = 80;
+        progressBox.y = 80;
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+
+        var width = game.config.width;
+        var height = game.config.height;
+        var loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        
+        this.load.on('progress', function (value) {
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+        });
+        
         this.load.image('particle', './assets/sprites/5x5_white.png');
         this.load.image('backdrop', './assets/tiles/repairsky.png');
         this.load.image('robot', './assets/sprites/bipedal-unit1.png')
@@ -84,8 +123,3 @@ class MainMenu extends Phaser.Scene {
         });
     }
 }
-
-
-
-
-
