b590620adbad276c7fc18c76188c0dceb6d4edda
ro-j0 <63885199+ro-j0@users.noreply.github.com>
2021-06-07 17:33:52 -0700

    Update MainMenu.js

---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,37 @@
+class MainMenu extends Phaser.Scene {
+
+    constructor() {
+        super("mainMenuScene");
+    }
+
+    create() {
+        // Add Play Button to the Screen
+        this.play = this.add.text(game.config.width/2 , 3*game.config.height/4, "PLAY", CREDITSConfig).setOrigin(0.5, 0.5);
+        this.play.setInteractive();
+        this.play.on('pointerover', () => { enterButtonHoverState(this.play); });
+        this.play.on('pointerout', () => { enterButtonRestState(this.play); });
+        this.play.on('pointerdown', () => { 
+            this.scene.start("lab"); 
+        });
+
+        // Add Credits Button to the Screen
+        // this.tutorial = this.add.text(game.config.width/3 , 3*game.config.height/4, "TUTORIAL", CREDITSConfig).setOrigin(0.5, 0.5);
+        // this.tutorial.setInteractive();
+        // this.tutorial.on('pointerover', () => { enterButtonHoverState(this.tutorial); });
+        // this.tutorial.on('pointerout', () => { enterButtonRestState(this.tutorial); });
+        // this.tutorial.on('pointerdown', () => { 
+        //     this.UISound.play();
+        //     this.scene.start("tutorialScene"); 
+        // });
+
+        // Add Credits Button to the Screen
+        this.credits = this.add.text((2 * game.config.width)/3 -20, 3*game.config.height/4, "CREDITS", CREDITSConfig).setOrigin(0.5, 0.5);
+        this.credits.setInteractive();
+        this.credits.on('pointerover', () => { enterButtonHoverState(this.credits); });
+        this.credits.on('pointerout', () => { enterButtonRestState(this.credits); });
+        this.credits.on('pointerdown', () => { 
+            this.UISound.play();
+            this.scene.start("creditsScene");
+        });
+    }
+}
